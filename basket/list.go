479ea89739fb62c914c8b5df76ba5cfc49ee8bda
rishi <rishikeshkrupa@gmail.com>
2020-09-19 08:15:21 +0000

    New Release Version 2.0.0

--- basket/list.go
+++ basket/list.go
@@ -1,13 +1,22 @@
 package basket
 
+// we restrict it to only certain fruits
+
+var ResFruits = make(map[string]bool)
+
+func init() {
+	ResFruits["apple"] = true
+	ResFruits["orange"] = true
+}
+
 type Fruit struct {
 	Name  string
 	Color string
 }
 
-func Fruiter(n, c string) *Fruit {
+func Fruiter(n, c string) Fruit {
 
 	f := Fruit{Name: n, Color: c}
-	return &f
+	return f
 
 }
--- basket/propfmt.go
+++ basket/propfmt.go
@@ -1,7 +1,18 @@
 package basket
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func (s Fruit) Fruitfmt() {
-	fmt.Printf("%s is in %s color\n", s.Name, s.Color)
+func (s Fruit) Fruitfmt() (string, error) {
+
+	_, present := ResFruits[s.Name]
+
+	if present {
+		return fmt.Sprintf("%s is in %s color\n", s.Name, s.Color), nil
+	} else {
+		return "", errors.New("Not in list")
+	}
+
 }
